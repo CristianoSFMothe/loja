4194cfe56758d67a56b83d2960bd063cea0a9a80
Cristiano da Silva Ferreira <68359459+CristianoSFMothe@users.noreply.github.com>
2022-11-13 17:19:48 -0300

    :rocket: feat: Refatoração....

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ProdutoModule } from './produto/produto.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [UserModule, ProdutoModule],
 })
 export class AppModule {}
